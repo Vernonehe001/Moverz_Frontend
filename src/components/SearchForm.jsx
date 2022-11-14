5780392397674cf5dd3dfe494102ec805f565193
kwamekyeimonies <kwamekyeimonies@gmail.com>
2022-11-14 15:50:46 +0000

    search form update

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import "./SearchForm.css";
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button';
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 import Axios from '../Axios';
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
+
 
 const SearchForm = () => {
 
@@ -15,37 +16,91 @@ const SearchForm = () => {
   const [loading, setLoading] = useState(true)
   const [productId, setProductId] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [err, setErr] = useState('')
-
-
-  const handleproductId = event=>{
-      setProductId(event.target.value)
-  }
 
  
-  const getProductItem = event=>{
-    event.preventDefault();
-    setLoading(true);
 
-    Axios(`/api/products/${productId}`)
-    .then((response) => {
-      setProduct(response.data)
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-    .finally(()=>setLoading(false))
+  //635e954680ce1128231e760b
+  const handleproductId = event=>{
+    setProductId(event.target.value)
   }
+
+  console.log(productId)
+
+  const getProductItem = (e)=> {
+    e.preventDefault()
+    if(productId && productId.length >0){
+      fetch(`https://moverzbackend.herokuapp.com/api/products/${productId}`)
+    .then((response)=> response.json())
+    .then((json)=>setProduct(json))
+    .catch((error)=> console.log(error))
+    .finally(()=>setLoading(false))
+    }
+    else{
+      <p>Enter product ID</p>
+    }
+  }
+
 
   console.log(product)
 
-   /*
+/*
 
+  useEffect(()=>{
+    const fetchProduct = async() =>{
+      try{
+        const response = await Axios.get("/api/products");
+        setProduct(response.data)
+      }
+      catch(err){
+        if(err.response){
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        }
+        else{
+          console.log(`Error: ${err.message}`)
+        }
+      }
+    }
+
+    fetchProduct();
+  },[])
+
+  console.log(product)
+
+ 
+ 
+
+   const getProdutById= (_id) =>{
+    _id = productId
+    const productList = product.filter((post)=>post._id !== _id);
+    setProduct(productList);
+  }
+
+   
+
+  console.log(product)
+
+  
+  useEffect(()=>{
+    fetchData()
+    
+  },[])
+
+  const fetchData = ()=> {
+    fetch("https://moverzbackend.herokuapp.com/api/products/")
+    .then((response)=> response.json())
+    .then((json)=>setProduct(json))
+    .catch((error)=> console.log(error))
+  }
+
+  console.log(product)
+  
+
+   
   const handleProduct = async(e) =>{
     e.preventDefault();
-
     setLoading(true);
-
     try{
       const response = await fetch(
         `http://localhost:8898/api/products/${productId}`,
@@ -56,18 +111,13 @@ const SearchForm = () => {
           },
         }
       );
-
       if (!response.ok){
         throw new Error (`Error! status: ${response.status}`)
       } 
-
       const result = await response.json();
-
       console.log('result is: ', JSON.stringify(result, null,4));
-
       setProduct(result);
     }
-
     catch(err){
       setErr(err.message);
     }
@@ -75,7 +125,6 @@ const SearchForm = () => {
       setIsLoading(false)
     }
   }
-
   console.log(product)
 */
 
@@ -108,35 +157,31 @@ const SearchForm = () => {
             </Form>
       </div>
           {
-            loading ?  (
-              <p className='loading_container'>Enter product's unique ID</p>
-            )
-                        
-            :
-            (
+            productId && productId?.length > 0 && productId === product._id?  (
+              
               <div className='product_item_container'>
               <Container className='product_main_container' >
                    <Col className='parent_sub-container'>
-                   <Col md="auto" className='product_id_header'><p>{product._id} </p></Col>   
-                   <Col md="auto" className='product_delivery-date'><p>{product.delivered_date} </p></Col>
+                   <Col md="auto" className='product_id_header'><p>Product ID:{product._id} </p></Col>   
+                   <Col md="auto" className='product_delivery-date'><p>Shippment Date:{product.delivered_date} </p></Col>
                    </Col>
 
 
                    <Row className='parent_row'>
                    <Row className='product_top'>                                     
-                    <Col md="auto" className='product_status'><p>{product.product_status} </p></Col>                    
+                    <Col md="auto" className='product_status'><p>Product Status{product.product_status} </p></Col>                    
                    </Row>
 
                    <Row className='country_row'>
-                   <Col><p>{product.product_source}</p></Col>
+                   <Col><p>Product Origin: {product.product_source}</p></Col>
                    </Row>
            
                   
                     <Row className='product_loc_container'>
-                    <Col>{product.product_packages} </Col>
+                    <Col>Product Content{product.product_packages} </Col>
                     <Col>{product.product_description} </Col>
 
-                    <Col>{product.product_destination} </Col>
+                    <Col>Product Set Destination: {product.product_destination} </Col>
                    </Row>
            
                    <Row className='product_address_container'>
@@ -162,6 +207,11 @@ const SearchForm = () => {
                        
               </Container>
               </div>
+            )
+                        
+            :
+            (
+              <p className='loading_container'>Enter product's unique ID</p>
             )
 
           }
